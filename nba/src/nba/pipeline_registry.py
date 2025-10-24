1ce6ce0bbe6f45f2ff944439d5aa9da800134979
SebastianNavarro1 <seb.navarrog@duocuc.cl>
2025-10-24 19:13:41 -0300

    Pipeline y Nodos del modelo de regresion

--- nba/src/nba/pipeline_registry.py
+++ nba/src/nba/pipeline_registry.py
@@ -3,16 +3,19 @@ from kedro.pipeline import Pipeline
 from .pipelines.data_engineering import pipeline as de_pipeline
 from .pipelines.union_tablas import pipeline as ut_pipeline
 from .pipelines.classification import pipeline as classification_pipeline
+from.pipelines.regression import pipeline as regression_pipeline
 
 def register_pipelines() -> dict[str, Pipeline]:
     data_engineering = de_pipeline.create_pipeline()
     union_tablas = ut_pipeline.create_pipeline()
     classification = classification_pipeline.create_pipeline()
+    regression = regression_pipeline.create_pipeline()
 
     return {
         "__default__": data_engineering + union_tablas + classification,
         "data_engineering": data_engineering,
         "union_tablas": union_tablas,
         "classification": classification,
-        "complete": data_engineering + union_tablas + classification
+        "complete": data_engineering + union_tablas + classification,
+        "regression": regression
     }
